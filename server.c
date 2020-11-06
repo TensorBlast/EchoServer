a560e5ff5cbcaf3189dd4d248b27bbeeda988a4d
ankit pasi <ankitpasi1@gmail.com>
2020-11-06 06:59:51 +0400

    First

---
+++ server.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define PORT "3490"
+
+#define BACKLOG 10
+
+#define BUFSIZE 100
+
+void sigchld_handler(int);
+void * get_in_addr(struct sockaddr *);
+void receivehandler(int sock);
+
+
+int main(void)
+{
+	int sockfd, new_fd;
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr;
+
+	char * hello_msg = "Hello, World!\n";
+
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes = 1;
+	char s[INET6_ADDRSTRLEN];
+
+	int rv;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	if((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(errno));
+		return 1;
+	}
+
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+			perror("server: socket");
+			continue;
+		}
+
+		if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+		break;
+
+	}
+
+	freeaddrinfo(servinfo);
+
+	if (p == NULL) {
+		fprintf(stderr, "server: failed to bind\n");
+		exit(1);
+	}
+
+	if(listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	sa.sa_handler = sigchld_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if(sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+
+	printf("server: waiting for connection...\n");
+
+	while(1) {	//main accept() loop
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1) {
+			perror("accept");
+			continue;
+		}
+
+		inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s,\
+			sizeof s);
+
+		printf("server: got connection from %s\n", s);
+
+		if(!fork()) {
+			close(sockfd);
+		
+			if(send(new_fd, (void *)hello_msg, strlen(hello_msg), 0) == -1)
+				perror("send");
+
+			if(send(new_fd, "Enter characters(q to quit): ", 29, 0) == -1)
+				perror("send");
+
+			receivehandler(new_fd);
+
+			close(new_fd);
+			exit(0);
+		}
+
+		close(new_fd);
+	}
+
+	return 0;
+}
+
+void receivehandler(int sock)
+{
+	char buf[BUFSIZE];
+	int lenrecvd;
+
+	while(1) {
+		memset(&buf, '\0', BUFSIZE);
+		if((lenrecvd = recv(sock, (void *) buf, BUFSIZE, 0)) == -1)
+				perror("recv");
+
+
+		buf[lenrecvd-2]='\0';
+
+		if (strcmp(buf, "q")==0)
+			return;
+
+		char *msg = strcat("You sent: ", buf);
+		int len = strlen(msg);
+		printf("%d\n", len);
+		if (send(sock, (void*)msg, len, 0) == -1)
+			perror("send");
+		printf("Received: %s\n", buf);
+	}
+}
+
+void sigchld_handler(int s)
+{
+	int saved_errno = errno;
+
+	while(waitpid(-1, NULL, WNOHANG)>0);
+
+	errno = saved_errno;
+}
+
+void *get_in_addr(struct sockaddr *sa)
+{
+	if(sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in *)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
